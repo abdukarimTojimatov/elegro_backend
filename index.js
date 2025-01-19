4a84b97cd41114205305869f8a42f9671e303b6a
Tojimatov Abdukarim <walterbriendeveloper@gmail.com>
2025-01-19 11:05:09 +0500

    feat: changed portfolio

--- index.js
+++ index.js
@@ -130,17 +130,37 @@ const startServer = async () => {
   try {
     await server.start();
 
-    const frontendOrigin = isProduction
-      ? 'http://91.108.122.60:3002'
-      : 'http://localhost:3002';
+    const allowedOrigins = [
+      'http://91.108.122.60:3002',
+      'http://localhost:3002',
+      'http://91.108.122.60',
+      'http://localhost:3000',
+    ];
 
     // Apply middleware
     app.use(
       '/graphql',
-      requestLogger, // Add request logging middleware
+      requestLogger,
       cors({
-        origin: [frontendOrigin],
+        origin: function (origin, callback) {
+          // Allow requests with no origin (like mobile apps or curl requests)
+          if (!origin) return callback(null, true);
+
+          if (allowedOrigins.indexOf(origin) !== -1) {
+            callback(null, true);
+          } else {
+            console.log('Blocked origin:', origin);
+            callback(new Error('Not allowed by CORS'));
+          }
+        },
         credentials: true,
+        methods: ['POST', 'OPTIONS'],
+        allowedHeaders: [
+          'Content-Type',
+          'Authorization',
+          'X-Requested-With',
+          'Accept',
+        ],
       }),
       express.json(),
       expressMiddleware(server, {
