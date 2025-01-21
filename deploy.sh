2b98b5fef6f2207ca7ab5ec445c56306ef840042
Tojimatov Abdukarim <walterbriendeveloper@gmail.com>
2025-01-21 12:28:22 +0500

    feat: changed portfolio

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ git checkout master
 git reset --hard
 git pull origin master
 npm i
-pm2 start process.config.cjs --env production 
+pm2 start process.config.cjs --env development
 
 # DEVELOPMENT
 # pm2 start process.config.js --env development
